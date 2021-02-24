52e36aab243019c968afc848c60fb59e56a77d45
shellyyg <chih-ching.yang@whu.edu>
2021-02-24 22:40:52 +0100

    commit 7 again

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const fistCommit = 'Hi I am first commit';
 const Commit4 = '4 fix';
 const Commit5 = '5';
-<<<<<<< HEAD
 const Commit6 = '6 add add';
-
-=======
-const Commit6 = '6 add';
-const Commit7 = '7';
->>>>>>> f7cc2cb... commit 7
+const Commit7 = '7 again now I change sth';
