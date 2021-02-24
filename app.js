5b9ebf1a60664c98093111bf00e3d17ccbbeaa54
shellyyg <chih-ching.yang@whu.edu>
2021-02-24 22:39:56 +0100

    commit fix

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const fistCommit = 'Hi I am first commit';
 const Commit4 = '4 fix';
 const Commit5 = '5';
+<<<<<<< HEAD
 const Commit6 = '6 add add';
 
+=======
+const Commit6 = '6 add';
+const Commit7 = '7';
+>>>>>>> f7cc2cb... commit 7
